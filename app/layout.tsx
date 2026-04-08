6bbf492ca7987eaa776186de41b09360f542caf4
mohnndZaghloul <mohanndzaghloul17@gmail.com>
2026-04-08 11:03:51 +0200

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,7 @@ export default function RootLayout({
           disableTransitionOnChange>
           <NavBar />
           {children}
+          footer
         </ThemeProvider>
       </body>
     </html>
