3361a2e76b96db6013fe622efa5912b4be65845f
mohnndZaghloul <mohanndzaghloul17@gmail.com>
2026-04-24 15:03:04 +0300

    test dashboard page

--- app/dashboard/Page.tsx
+++ app/dashboard/Page.tsx
@@ -7,53 +7,54 @@ import { Box, HeartIcon } from "lucide-react";
 import { Metadata } from "next";
 import { unauthorized } from "next/navigation";
 
-export const metadata: Metadata = {
-  title: "Dashboard | Next Store",
-  description:
-    "dashboard to control products orders and customers by admin and users",
-  keywords: ["dashboard"],
-};
+// export const metadata: Metadata = {
+//   title: "Dashboard | Next Store",
+//   description:
+//     "dashboard to control products orders and customers by admin and users",
+//   keywords: ["dashboard"],
+// };
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
 export default async function DashboardPage() {
-  const user = await getCurrentUser();
-  if (!user) unauthorized();
+  // const user = await getCurrentUser();
+  // if (!user) unauthorized();
 
-  const productsData = await getCurrentUserProducts();
-  const favProducts = await getFavProducts();
-  const favProductsId = new Set(favProducts.map((fav) => fav.productId));
+  // const productsData = await getCurrentUserProducts();
+  // const favProducts = await getFavProducts();
+  // const favProductsId = new Set(favProducts.map((fav) => fav.productId));
   return (
-    <div className="container mb-4 space-y-2 md:space-y-4">
-      <div className="grid gap-4 lg:grid-cols-2">
-        <StaticCard
-          title="owner products"
-          staticName="products"
-          staticNumber={productsData.length.toString()}
-          icon={<Box className="fill-primary text-primary" size={32} />}
-        />
-        <StaticCard
-          title="favorites"
-          staticName="items"
-          staticNumber={favProducts.length.toString()}
-          icon={<HeartIcon className="fill-primary text-primary" size={32} />}
-        />
-      </div>
+    // <div className="container mb-4 space-y-2 md:space-y-4">
+    //   <div className="grid gap-4 lg:grid-cols-2">
+    //     <StaticCard
+    //       title="owner products"
+    //       staticName="products"
+    //       staticNumber={productsData.length.toString()}
+    //       icon={<Box className="fill-primary text-primary" size={32} />}
+    //     />
+    //     <StaticCard
+    //       title="favorites"
+    //       staticName="items"
+    //       staticNumber={favProducts.length.toString()}
+    //       icon={<HeartIcon className="fill-primary text-primary" size={32} />}
+    //     />
+    //   </div>
 
-      <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        {productsData.map((product) => {
-          const isFavorite = favProductsId.has(product.id);
-          return (
-            <ProductCard
-              key={product.id}
-              product={product}
-              isShopping={false}
-              isFavorite={isFavorite}
-              isLoggedIn={!!user}
-            />
-          );
-        })}
-      </div>
-    </div>
+    //   <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+    //     {productsData.map((product) => {
+    //       const isFavorite = favProductsId.has(product.id);
+    //       return (
+    //         <ProductCard
+    //           key={product.id}
+    //           product={product}
+    //           isShopping={false}
+    //           isFavorite={isFavorite}
+    //           isLoggedIn={!!user}
+    //         />
+    //       );
+    //     })}
+    //   </div>
+    // </div>
+    <div>Dashboard Works</div>
   );
 }
