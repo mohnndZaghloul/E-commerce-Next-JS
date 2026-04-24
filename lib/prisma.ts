fa54d8b9e17ffa1670987bbf19a753f4e2206dd0
mohnndZaghloul <mohanndzaghloul17@gmail.com>
2026-04-24 04:45:01 +0300

    prisma update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,13 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
+import { Pool } from "pg";
 
 const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, // DATABASE_URL للـ queries
+});
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
