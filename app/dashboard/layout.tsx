0732d6902ab492854e53259346e3c5e2e7cce18c
mohnndZaghloul <mohanndzaghloul17@gmail.com>
2026-04-24 14:04:04 +0300

    force dynamic for layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,6 +8,8 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 
+export const dynamic = "force-dynamic";
+
 export default async function DashboardLayout({
   children,
 }: Readonly<{
