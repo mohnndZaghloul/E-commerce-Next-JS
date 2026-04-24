0518e3323ef72d03785e2d03f2d9ae11760c16c0
mohnndZaghloul <mohanndzaghloul17@gmail.com>
2026-04-24 15:07:36 +0300

    test dashboard page

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -16,21 +16,21 @@ export default async function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-vertical:h-4 data-vertical:self-auto"
-            />
-            <DashboardBreadcrumb />
-          </div>
-        </header>
-        {children}
-      </SidebarInset>
-    </SidebarProvider>
+    // <SidebarProvider>
+    //   <AppSidebar />
+    //   <SidebarInset>
+    //     <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+    //       <div className="flex items-center gap-2 px-4">
+    //         <SidebarTrigger className="-ml-1" />
+    //         <Separator
+    //           orientation="vertical"
+    //           className="mr-2 data-vertical:h-4 data-vertical:self-auto"
+    //         />
+    //         <DashboardBreadcrumb />
+    //       </div>
+    //     </header>
+    <div>{children}</div>
+    //   </SidebarInset>
+    // </SidebarProvider>
   );
 }
