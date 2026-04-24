840a4dc8715e4c73507539f207669e4870695137
mohnndZaghloul <mohanndzaghloul17@gmail.com>
2026-04-24 15:21:59 +0300

    problem was at dashboard/page.tsx the name of file in github was Page not page

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,29 +8,27 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 
-export const dynamic = "force-dynamic";
-
 export default async function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    // <SidebarProvider>
-    //   <AppSidebar />
-    //   <SidebarInset>
-    //     <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-    //       <div className="flex items-center gap-2 px-4">
-    //         <SidebarTrigger className="-ml-1" />
-    //         <Separator
-    //           orientation="vertical"
-    //           className="mr-2 data-vertical:h-4 data-vertical:self-auto"
-    //         />
-    //         <DashboardBreadcrumb />
-    //       </div>
-    //     </header>
-    <div>{children}</div>
-    //   </SidebarInset>
-    // </SidebarProvider>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-vertical:h-4 data-vertical:self-auto"
+            />
+            <DashboardBreadcrumb />
+          </div>
+        </header>
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
